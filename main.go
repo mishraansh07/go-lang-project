84f18e58c78d7209460da502f07164ff85157f10
Ansh Mishra <theansh63@gmail.cpm>
2025-12-12 19:18:32 +0530

    First go CLS Todo list

--- main.go
+++ main.go
@@ -1,33 +1,36 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
+
+type Student struct {
+	name string
+	task string
+	day  int
+}
 
 func main() {
-	var sum int64
 
-	fmt.Println("Hello, Welcome to main program")
-
-	var arr_1 [5]int
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for i := 0; i < 5; i++ {
-		fmt.Printf("Input the digits of array %d: ", i)
-		fmt.Fscan(reader, &arr_1[i])
+	// Create two students
+	stud1 := Student{
+		name: "Ansh Mishra",
+		task: "I want to work on my maths project",
+		day:  23,
 	}
 
-	fmt.Println("The new Array is:", arr_1)
-
-	for i := 0; i < 5; i++ {
-		sum += int64(arr_1[i])
+	stud2 := Student{
+		name: "Rahul Sharma",
+		task: "I want to complete my Go project",
+		day:  12,
 	}
-	fmt.Println("Sum:", sum)
 
-	avg := float64(sum) / 5
+	// Slice of Students
+	studentsList := []Student{stud1, stud2}
 
-	fmt.Println("The Average of the array inputs are:\n", avg)
+	// Print them
+	for _, s := range studentsList {
+		fmt.Println("Name:", s.name)
+		fmt.Println("Task:", s.task)
+		fmt.Println("Day:", s.day)
+		fmt.Println("------------------")
+	}
 }
