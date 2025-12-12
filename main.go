31c337ac9fd3c7719edb5cf4702c9eea471e3d4d
Ansh Mishra <theansh63@gmail.cpm>
2025-12-12 19:30:15 +0530

    Commiting in main.go

--- main.go
+++ main.go
@@ -1,36 +1,105 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
 
-type Student struct {
-	name string
-	task string
-	day  int
+	"github.com/aquasecurity/table"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
+func (t Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(t) {
+		return errors.New("index out of range")
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	t := *todos
+	todo := &t[index]
+
+	if !todo.Completed {
+		completedTime := time.Now()
+		todo.CompletedAt = &completedTime
+	} else {
+		todo.CompletedAt = nil
+	}
+
+	todo.Completed = !todo.Completed
+	(*todos)[index] = *todo
+
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created At", "Completed At")
+
+	for index, t := range *todos {
+		completed := "❌"
+		completedAt := ""
+
+		if t.Completed {
+			completed = "✅"
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC1123)
+			}
+		}
+
+		table.AddRow(
+			strconv.Itoa(index),
+			t.Title,
+			completed,
+			t.CreatedAt.Format(time.RFC1123),
+			completedAt,
+		)
+	}
+
+	table.Render()
 }
 
 func main() {
-
-	// Create two students
-	stud1 := Student{
-		name: "Ansh Mishra",
-		task: "I want to work on my maths project",
-		day:  23,
-	}
-
-	stud2 := Student{
-		name: "Rahul Sharma",
-		task: "I want to complete my Go project",
-		day:  12,
-	}
-
-	// Slice of Students
-	studentsList := []Student{stud1, stud2}
-
-	// Print them
-	for _, s := range studentsList {
-		fmt.Println("Name:", s.name)
-		fmt.Println("Task:", s.task)
-		fmt.Println("Day:", s.day)
-		fmt.Println("------------------")
-	}
+	todos := Todos{}
+	todos.add("Buy groceries")
+	todos.add("Study Go Lang")
+	todos.toggle(1)
+	todos.print()
 }
